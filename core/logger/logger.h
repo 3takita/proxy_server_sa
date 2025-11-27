a97542684462a734a2fe827b5b91c312ded2f0bb
DrChrisHax <cmanlove1234@outlook.com>
2025-11-26 16:48:18 -0800

    moved stuff

--- core/logger/logger.h
+++ core/logger/logger.h
@@ -12,7 +12,7 @@ public:
     void info(const std::string& msg);
     void warning(const std::string& msg);
     void error(const std::string& msg);
-    void critical(const std::string& msg)
+    void critical(const std::string& msg);
 
 private:
     std::ofstream file;
